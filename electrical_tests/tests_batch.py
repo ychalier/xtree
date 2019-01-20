9ef244d755712625f24c0819dca4fb63c9009e42
Fran-cois <francois.amat@telecom-paristech.fr>
2019-01-21 00:23:21 +0100

    new tests

---
+++ electrical_tests/tests_batch.py
@@ -0,0 +1,32 @@
+from skmultiflow.trees import HoeffdingTree
+from skmultiflow.evaluation.evaluate_prequential import EvaluatePrequential
+from skmultiflow.data.file_stream import FileStream
+
+
+from hatt import HATT
+
+dataset = "result_elec"
+
+# 1. Create a stream
+
+stream = FileStream("./"+dataset+".csv", n_targets=1, target_idx=-1)
+# 2. Prepare for use
+stream.prepare_for_use()
+
+
+h = [
+    HoeffdingTree(),
+	HATT(),
+]
+
+for i in range(0,100,10):
+	evaluator = EvaluatePrequential(
+	    pretrain_size=0,
+	    show_plot=True,
+	    max_samples=20000,
+	    metrics=['accuracy','kappa'],
+	    batch_size=i,
+        output_file="output_pretrain_0_batch_size_" + str(i) + "_dataset_"+dataset +".csv"
+	)
+
+	evaluator.evaluate(stream=stream, model=h, model_names=['HT', 'HATT'])
---
+++ electrical_tests/tests_pretrain.py
@@ -0,0 +1,31 @@
+from skmultiflow.trees import HoeffdingTree
+from skmultiflow.evaluation.evaluate_prequential import EvaluatePrequential
+from skmultiflow.data.file_stream import FileStream
+
+from hatt import HATT
+
+# 1. Create a stream
+dataset = "result_elec"
+
+stream = FileStream("./"+dataset+".csv", n_targets=1, target_idx=-1)
+# 2. Prepare for use
+stream.prepare_for_use()
+
+
+h = [
+    HoeffdingTree(),
+	HATT(),
+]
+
+for i in range(0,10000,100):
+	evaluator = EvaluatePrequential(
+	    pretrain_size=i,
+	    show_plot=True,
+	    max_samples=20000,
+	    metrics=['accuracy','kappa'],
+	    batch_size=1,
+      output_file="output_pretrain_"+ str(i)+"_batch_size_" + "1" + "_dataset_"+dataset + ".csv"
+
+	)
+
+	evaluator.evaluate(stream=stream, model=h, model_names=['HT', 'HATT'])
